0896ecb1dbb238433676e57f6533fac57d552d30
juxxsrck <justine.pulido@gmail.com>
2022-08-18 19:27:10 -0500

    primera parte del ejercicio, estructura de control "if", bucle "while"

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,30 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        int numeroIf = -10;
+        numero_if(numeroIf);
+
+        System.out.println();
+        System.out.println();
+
+        
+        int numeroWhile = -10;
+        numero_while(numeroWhile);
+    }
+
+    public static void numero_if(int numero){
+        if (numero > 0){
+            System.out.println("El numero 'numeroIf' es positivo: " + numero);
+        } else if (numero < 0) {
+            System.out.println("El numero 'numeroIf' es negativo: " + numero);
+        }else System.out.println("El numero 'numeroIf' es 0: " + numero);
+    }
+
+    public static void numero_while(int numero){
+        while(numero < 3){
+            System.out.println("El numero es: " + numero);
+            numero++;
+        }
+    }
+
+
 }

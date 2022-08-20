6b32d28460d827659c11173bf3e492078145ef1e
juxxsrck <justine.pulido@gmail.com>
2022-08-20 17:26:59 -0500

    Estructuras de control While, Do while, For

--- src/Main.java
+++ src/Main.java
@@ -6,9 +6,21 @@ public class Main {
         System.out.println();
         System.out.println();
 
-        
+
         int numeroWhile = -10;
         numero_while(numeroWhile);
+
+        System.out.println();
+        System.out.println();
+
+        int numeroDoWhile = 1;
+        numero_do_while(numeroDoWhile);
+
+        System.out.println();
+        System.out.println();
+
+        int numeroFor = 0;
+        numero_for(numeroFor);
     }
 
     public static void numero_if(int numero){
@@ -26,5 +38,16 @@ public class Main {
         }
     }
 
+    public static void numero_do_while(int numero){
+        do {
+            System.out.println("El numero es: " + numero);
+            numero++;
+        }while (numero == 3);
+    }
 
+    public static void numero_for(int numero){
+        for ( ; numero <= 3; numero++) {
+            System.out.println("El numero es: " + numero);
+        }
+    }
 }
